c8ea5582b4ca805eb790b5a90d16b7b73d42eca9
ALEXANDRU-NICOLAE PETRICA <alexandru.petrica@stud.ubbcluj.ro>
2024-11-26 16:41:14 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace PetricaAlexandruLab7
+﻿using PetricaAlexandruLab7.Data;
+using System;
+using System.IO;
+
+namespace PetricaAlexandruLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
